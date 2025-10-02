9c95c7c4b27e846e8278cdc324f2fe264d333802
Sachin Tiwari <sachinadeshtiwari1999@gmail.com>
2025-10-02 15:55:28 +0530

    ownership overview

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -1,20 +1,23 @@
-mod rule;
+//mod rule;
+mod test;
+
 // understanding of scoped variable
-const OTHER:u8 = 11;    // Global constant variable it thought accessable in program 
+//const OTHER:u8 = 11;    // Global constant variable it thought accessable in program 
 fn main() {
-    rule::rule(); // Calling another file
-    let out_var = 10;   // Accessable in this function
-    {
-        let in_var = 5;
-        println!("{}",in_var);
-        println!("{}",out_var);
-        
-    }// end of in_var scope
-    println!("{}",out_var);
-    printval();
+    //rule::rule(); // Calling another file
+    test::test();
+    // let out_var = 10;   // Accessable in this function
+    // {
+    //     let in_var = 5;
+    //     println!("{}",in_var);
+    //     println!("{}",out_var);
+
+    // }// end of in_var scope
+    // println!("{}",out_var);
+    // printval();
 }
 
-fn printval(){
-    println!("{}",OTHER);
-}
+// fn printval(){
+//     println!("{}",OTHER);
+// }
 
